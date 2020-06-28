1482ad1e168ec5654213c1bd4c72c5c66103077c
chuyiran <252964202@qq.com>
2020-06-28 18:15:10 +0800

    2020-06-28 框架与目录搭建

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+// module.exports={
+//     configureWebpack:{
+//         resolve:{
+
+//         }
+//     }
+// }
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
+            }
+        }
+    }
+}
